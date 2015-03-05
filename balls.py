560387ab206f61ae55888a4ffa8897d673b62d1f
Alex <spiridoncha@gmail.com>
2015-03-06 01:24:03 +0300

    added contacts of balls

--- balls.py
+++ balls.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python
+#/usr/bin/env python
 # coding: utf
 
+import math
 import pygame
 import random
+import itertools
 
 SIZE = 640, 480
 
@@ -87,15 +89,17 @@ class RotateBall(Ball):
         '''Create a ball from image'''
         Ball.__init__(self, filename, pos, speed)
         x, y = self.surface.get_size()
-        self.surface = pygame.transform.scale(self.surface, (int(scale*x), int(scale*y)))
         self.rect = self.surface.get_rect()
         self.angle = angle
         self.scale = scale
-        self.mask = pygame.mask.from_surface(self.surface)
+        self.mask = pygame.mask.from_surface(self.surface).scale((int(scale*x), int(scale*y)))
+        self.surface = pygame.transform.scale(self.surface, (int(scale*x), int(scale*y)))
+        self.mass = scale
 
     def action(self):
         if self.active:
-            self.surface = pygame.transform.rotate(self.surface, self.angle)
+            if random.random() > 0.3:
+                self.surface = pygame.transform.rotate(self.surface, self.angle)
             self.rect = self.surface.get_rect(center=self.rect.center)
             self.pos = self.pos[0]+self.speed[0], self.pos[1]+self.speed[1]
 
@@ -158,7 +162,6 @@ class GameWithDnD(GameWithObjects):
         return False
 
     def Events(self, event):
-        events = set([pygame.MOUSEBUTTONDOWN, pygame.MOUSEBUTTONUP, pygame.MOUSEMOTION])
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             click = self.locate(event.pos)
             self.oldposInBall = False
@@ -177,25 +180,49 @@ class GameWithDnD(GameWithObjects):
                 self.drag = None
         GameWithObjects.Events(self, event)
 
-class GameWithDnDAndGravity(GameWithDnD):
+class GameWithDnDAndGravityAndContact(GameWithDnD):
     def Events(self, event):
-        print self.objects[0].speed
         for obj in self.objects:
             if obj.active and event.type == Game.tickevent:
-                obj.speed = (obj.speed[0], obj.speed[1] + 0.1) 
+                obj.speed = (obj.speed[0], obj.speed[1] + 0.5) 
         GameWithDnD.Events(self, event)
+
+    def Logic(self, surface):
+        #contact
+        for obj in itertools.combinations(self.objects, 2):
+            doff0 = obj[0].mask.centroid()[0] - obj[1].mask.centroid()[0]
+            doff1 = obj[0].mask.centroid()[1] - obj[1].mask.centroid()[1]
+            boff0 = obj[0].pos[0] - obj[1].pos[0]
+            boff1 = obj[0].pos[1] - obj[1].pos[1]
+            offset = (int(doff0 + boff0), int(doff1 + boff1))
+            if obj[0].mask.overlap_area(obj[1].mask, offset):
+                theta = math.atan2(obj[0].pos[1] - obj[1].pos[1], obj[0].pos[0] - obj[1].pos[0])
+                cosTheta = math.cos(theta)
+                sinTheta = math.sin(theta)
+                x0_new = (obj[0].speed[0]*cosTheta + obj[0].speed[1]*sinTheta)
+                y0_new = (obj[0].speed[1]*cosTheta - obj[0].speed[0]*sinTheta)
+                x1_new = (obj[1].speed[0]*cosTheta + obj[1].speed[1]*sinTheta)
+                y1_new = (obj[1].speed[1]*cosTheta - obj[1].speed[0]*sinTheta)
+                v0 = (obj[0].mass - obj[1].mass)*x0_new + 2*obj[1].mass*x1_new 
+                v1 = 2*obj[0].mass*x0_new + (obj[1].mass - obj[0].mass)*x1_new
+                x0_new = v0/(obj[0].mass + obj[1].mass)
+                x1_new = v1/(obj[0].mass + obj[1].mass)
+                obj[0].speed = (x0_new*cosTheta - y0_new*sinTheta), (y0_new*cosTheta + x0_new*sinTheta)
+                obj[1].speed = (x1_new*cosTheta - y1_new*sinTheta), (y1_new*cosTheta + x1_new*sinTheta)
+                obj[0].pos = (obj[0].pos[0] + obj[0].speed[0], obj[0].pos[1] + obj[0].speed[1])
+                obj[1].pos = (obj[1].pos[0] + obj[1].speed[0], obj[1].pos[1] + obj[1].speed[1])
+        GameWithDnD.Logic(self, surface)
 
 Init(SIZE)
 Game = Universe(50)
 
-Run = GameWithDnDAndGravity()
+Run = GameWithDnDAndGravityAndContact()
 for i in xrange(5):
     x, y = random.randrange(screenrect.w), random.randrange(screenrect.h)
-    #dx, dy = 1+random.random()*5, 1+random.random()*5
     dx , dy = 0, 0
     angle = 90
     scale = 0.3 + random.random()
-    Run.objects.append(RotateBall("ball.gif",(x,y),(dx,dy), angle, scale))
+    Run.objects.append(RotateBall("ball.gif", (x,y), (dx,dy), angle, scale))
 
 Game.Start()
 Run.Init()
